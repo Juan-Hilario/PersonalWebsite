19439be6f8546031a55074895516c18675750d75
Juan-Hilario <juanhilario5645@gmail.com>
2024-07-10 23:42:46 -0400

    A bug fix on home page and little fixes
    Home page waffle button would not work because of invisible element; deleted element.
    Made empty portfolio page nicer to look at.
    Added picture of me to replace demo picture.
    Renamed Home.html to Index.html lol.

--- PersonalWebsite/htmlManager/views.py
+++ PersonalWebsite/htmlManager/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-   return render(request, "htmlManager/Home.html",
+   return render(request, "htmlManager/index.html",
                  {"title": "Home"})
 
 def portfolio(request):
